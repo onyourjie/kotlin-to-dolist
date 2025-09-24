21e64918e549ad3c3ac5b5fee15b2c5052265145
onyourjie <filzahmufidah@student.ub.ac.id>
2025-09-24 23:41:41 +0700

    feat: slicing input screen

---
+++ app/src/main/java/com/example/todolist/ui/add/AddTaskScreen.kt
@@ -0,0 +1,73 @@
+package com.example.todolist.ui.add
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.todolist.model.ToDoAppViewModel
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun AddTaskScreen(
+    viewModel: ToDoAppViewModel
+) {
+    var taskTitle by remember { mutableStateOf("") }
+    var taskDescription by remember { mutableStateOf("") }
+
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = { Text(text = "Add Task") },
+                navigationIcon = {
+                    IconButton(onClick = { /*TODO*/ }) {
+                        Icon(imageVector = Icons.AutoMirrored.Filled.ArrowBack, contentDescription = "Back")
+                    }
+                },
+                actions = {
+                    TextButton(onClick = { /*TODO*/ }) {
+                        Text(text = "Save")
+                    }
+                }
+            )
+        }
+    ) { innerPadding ->
+        Column(modifier = Modifier
+            .padding(innerPadding)
+            .padding(16.dp)
+        ) {
+            TextField(
+                value = taskTitle,
+                onValueChange = { taskTitle = it },
+                label = { Text("Task Title") },
+                modifier = Modifier.fillMaxWidth()
+            )
+
+            Spacer(modifier = Modifier.height(16.dp))
+
+            TextField(
+                value = taskDescription,
+                onValueChange = { taskDescription = it },
+                label = { Text("Task Description") },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(200.dp),
+                maxLines = 10,
+                minLines = 5
+            )
+        }
+    }
+}
+
+@Preview
+@Composable
+fun AddTaskScreenPreview() {
+    AddTaskScreen(viewModel = ToDoAppViewModel())
+}
