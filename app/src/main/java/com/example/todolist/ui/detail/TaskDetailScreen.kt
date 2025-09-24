1eb9f8409210d2ccb4765db779578dafc55331d7
onyourjie <filzahmufidah@student.ub.ac.id>
2025-09-24 23:15:07 +0700

    feat: slicing detail screen

---
+++ app/src/main/java/com/example/todolist/ui/detail/TaskDetailScreen.kt
@@ -0,0 +1,56 @@
+package com.example.todolist.ui.detail
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material3.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.todolist.model.ToDoAppViewModel
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun TaskDetailScreen (
+    viewModel: ToDoAppViewModel,
+    taskId: String?
+) {
+    val task = viewModel.getTaskById(taskId)
+
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = { Text(text = task?.title ?: "") },
+                navigationIcon = {
+                    IconButton(onClick = { /*TODO*/ }) {
+                        Icon(imageVector = Icons.AutoMirrored.Filled.ArrowBack, contentDescription = "Back")
+                    }
+                },
+                actions = {
+                    TextButton(onClick = { /*TODO*/ }) {
+                        Text(text = "Delete")
+                    }
+                }
+            )
+        }
+    ) { innerPadding ->
+        Column(modifier = Modifier.padding(innerPadding)) {
+            Text(
+                text = task?.description ?: "",
+                style = MaterialTheme.typography.bodyMedium,
+                modifier = Modifier.padding(16.dp)
+                    .padding(top = 16.dp)
+                    .fillMaxSize()
+            )
+        }
+    }
+}
+
+@Preview
+@Composable
+fun TaskDetailScreenPreview() {
+    TaskDetailScreen(viewModel = ToDoAppViewModel(), taskId = "2")
+}
